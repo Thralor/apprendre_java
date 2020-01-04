4326b81cf95a9d4ce9122380a2a167253c861e2f
Arnaud Marie <arnaudmarie77500@live.fr>
2020-01-04 17:57:13 +0100

    les boucles

--- src/main/java/com/github/arnaud/App.java
+++ src/main/java/com/github/arnaud/App.java
@@ -1,37 +1,154 @@
 package com.github.arnaud;
+import java.io.IOException;
 import java.util.*;
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-    	//Déclaration variables
-    	int nbCB = 0, nbCheque = 0, nbVirement = 0, nbDebit = 0;
-    	double prctCB = 0, prctCh = 0, prctVi = 0;
+// Déclaration des variables du menu
+    	String Creacpt = "1. Créer un compte", Affcpt = "2. Afficher un compte", Crealgn = "3. Créer une ligne comptable",
+    		   Sortir = "4. Sortir", Aide = "5. De l'aide", Choisir = "Veuillez faire votre choix : ";
+    		   		
     	
-    	Scanner lectureClavier = new Scanner(System.in);
+    	String Men1_Typcpt = "Type du compte [Types possibles : courant, joint, épargne] : ",
+    			Men1_Numcpt = "Numéro du compte : ",
+    			Men1_Cred = "Première valeur créditée : ",
+    			Men1_Taux = "Taux d'épargne : ",
+    			Men1_Typcpt_choix = "",
+    			Men1_Numcpt_choix = "",
+    			Men1_Cred_choix = "",
+    			Men1_Taux_choix = "";
+    			
+    			
+    			
     	
-    	//Demande les informations
-    	System.out.print(" Nombre de paiement par CB :");
-    	nbCB = lectureClavier.nextInt();
+    	String Men2_Numcpt = "Saisissez le N° de compte a consulter : ";
     	
-    	System.out.print(" Nombre de paiement par Chèque :");
-    	nbCheque = lectureClavier.nextInt();
+    	String Men3_Msgerr = "option non programmée";
     	
-    	System.out.print(" Nombre de paiement par Virement :");
-    	nbVirement = lectureClavier.nextInt();
+    	Byte choix = 0;
+    	char validation = '\0';
     	
-    	//Effectue les calculs
-    	nbDebit = nbCB+nbCheque+nbVirement;
-    	prctCB = (double) nbCB/(nbDebit) * 100;
-    	prctCh = (double) nbCheque/(nbDebit) * 100;
-    	prctVi = (double) nbVirement/(nbDebit) * 100;
     	
-    	//Affiche les resultats
-    	System.out.println("Vous avez émis "+nbDebit+ " débit(s) dont : ");
-    	System.out.println(prctCB+"% par Carte bleu");
-    	System.out.println(prctCh+"% par Chèque(s)");
-    	System.out.println(prctVi+"% par Virement(s)");
+    	Scanner Lectureclavier = new Scanner(System.in);
+ //Boucle principale
+    do
+    {
+ // Réinitialisation
+    	validation = '\0';
+    	choix = 0;
+ // Affichage du Menu princiapl
+    	System.out.println(Creacpt);
+    	System.out.println(Affcpt);
+    	System.out.println(Crealgn);
+    	System.out.println(Sortir);
+    	System.out.println(Aide);
+    	System.out.println(Choisir);
+    	
+ // Attent le choix de l'utlisateur
+    	choix = Lectureclavier.nextByte();
+    	
+ // Selon le choix on va dans le menu correspondant
+    	
+    	switch (choix)
+    	{
+    		case 1:
+    		  while(validation != '\n'&& validation != '\r')
+    		  {
+    			//Rentre le type de compte
+    			System.out.println(Men1_Typcpt);
+    			while(validation != '\n'&& validation != '\r') 
+    			{
+    				validation = (char)System.in.read();
+    				if (validation != '\n' && validation != '\r')
+    				{
+    					Men1_Typcpt_choix = Men1_Typcpt_choix + validation;
+    				}
+    			}
+    			validation = '\0';
+    			//Rentre le numéro de compte
+    			System.out.println(Men1_Numcpt);
+    			while(validation != '\n'&& validation != '\r') 
+    			{
+    				validation = (char)System.in.read();
+    				if (validation != '\n' && validation != '\r')
+    				{
+    					Men1_Numcpt_choix = Men1_Numcpt_choix + validation;
+    				}
+    			}
+    			validation = '\0';
+    			//Rentre le premier crédit
+    			System.out.println(Men1_Cred);
+    			while(validation != '\n'&& validation !='\r') 
+    			{
+    				validation = (char)System.in.read();
+    				if (validation != '\n' && validation != '\r')
+    				{
+    					Men1_Cred_choix = Men1_Cred_choix + validation;
+    				}
+    			}
+    			validation = '\0';
+    			//Rentre le taux
+    			System.out.println(Men1_Taux);
+    			while(validation != '\n'&& validation != '\r') 
+    			{
+    				validation = (char)System.in.read();
+    				if (validation != '\n' && validation != '\r')
+    				{
+    					Men1_Taux_choix = Men1_Taux_choix + validation;
+    				}
+    			}
+    			validation = '\0';
+    			//Récap
+    			System.out.println(Men1_Typcpt+Men1_Typcpt_choix);
+    	    	System.out.println(Men1_Numcpt+Men1_Numcpt_choix);
+    	    	System.out.println(Men1_Cred+Men1_Cred_choix);
+    	    	System.out.println(Men1_Taux+Men1_Taux_choix);
+    	    	System.out.println(" ");
+    	    	System.out.println("Si les informations sont valides appuyez sur entrée");
+    	    	validation = (char)System.in.read();
+    		  }
+    			
+  
+    		break;
+    		case 2:
+    			System.out.println(Men2_Numcpt);
+    		
+    		break;
+    		case 3:
+    			while(validation != '\n'&& validation != '\r') 
+    			{
+    				System.out.println(Men3_Msgerr);
+    				System.out.println("Appuyez sur entrée pour revenir au menu principal");
+    				validation = (char) System.in.read();
+    			}
+	    		
+	    	break;
+    		case 4:
+    			System.exit(0);
+    		break;
+    		case 5:
+    			while(validation != '\n' && validation != '\r') 
+    			{
+    				System.out.println(Creacpt+ " : Vous permez de créer un compte avec un premier crédit à un taux choisi");
+    	    		System.out.println(Affcpt+" : Vous permez d'obtenir les informations d'un compte précis");
+    	    		System.out.println(Crealgn+" : Vous permez de créditer ou débiter un compte déjà existant");
+    	    		System.out.println(Sortir+" : Vous permez de sortir");
+    	    		System.out.println("Appuyez sur entrée pour revenir au menu principal");
+    	    		validation = (char) System.in.read();
+    			}
+    		break;
+    		default:
+    			System.out.println("Menu non existant, veuillez réessayer");
+    			    		
+    	}
+    	
+    }while(choix != 4);
+    	
+    	
+    	
+    	
     	
     	
     	
---
+++ src/test/java/Chapitre_1.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Chapitre_1 {
+
+	public static void main(String[] args) {
+		
+		
+    	//Déclaration variables
+    	int nbCB = 0, nbCheque = 0, nbVirement = 0, nbDebit = 0;
+    	double prctCB = 0, prctCh = 0, prctVi = 0;
+    	
+    	Scanner lectureClavier = new Scanner(System.in);
+    	
+    	//Demande les informations
+    	System.out.print(" Nombre de paiement par CB :");
+    	nbCB = lectureClavier.nextInt();
+    	
+    	System.out.print(" Nombre de paiement par Chèque :");
+    	nbCheque = lectureClavier.nextInt();
+    	
+    	System.out.print(" Nombre de paiement par Virement :");
+    	nbVirement = lectureClavier.nextInt();
+    	
+    	//Effectue les calculs
+    	nbDebit = nbCB+nbCheque+nbVirement;
+    	prctCB = (double) nbCB/(nbDebit) * 100;
+    	prctCh = (double) nbCheque/(nbDebit) * 100;
+    	prctVi = (double) nbVirement/(nbDebit) * 100;
+    	
+    	//Affiche les resultats
+    	System.out.println("Vous avez émis "+nbDebit+ " débit(s) dont : ");
+    	System.out.println(prctCB+"% par Carte bleu");
+    	System.out.println(prctCh+"% par Chèque(s)");
+    	System.out.println(prctVi+"% par Virement(s)");
+
+
+	}
+
+}
